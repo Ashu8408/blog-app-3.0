437c310a4f089dfc2be6cd5e3b37eced673dc255
ashuOO7 <ashjnavj@gmail.com>
2025-01-31 20:21:43 +0530

    1. controller and model for comments 2. adding post has_many comments association

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2025_01_31_041800) do
+ActiveRecord::Schema[8.0].define(version: 2025_01_31_144853) do
+  create_table "comments", force: :cascade do |t|
+    t.string "name"
+    t.text "comment"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
   create_table "posts", force: :cascade do |t|
     t.string "title"
     t.text "content"
