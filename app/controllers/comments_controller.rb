9eb52f95892ebdd088b595cf459402e6b3b81d8c
ashuOO7 <ashjnavj@gmail.com>
2025-02-01 09:48:13 +0530

    soft delete comments

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,29 +1,41 @@
 class CommentsController < ApplicationController
+  before_action :set_post
+
+  def index
+    @all_comments = @post.comments.where(is_deleted: false)
+  end
   def create
     @post = Post.find(params[:post_id])
-     @comment = @post.comments.create(comment_params)
+    @comment = @post.comments.create(comment_params)
     redirect_to post_path(@post)
   end
 
   def destroy
+    Rails.logger.info("delete me aaya ")
     @post = Post.find(params[:post_id])
     @comment = @post.comments.find(params[:id])
     @comment.destroy
     redirect_to post_path(@post)
   end
 
-  # def destroy
-  #   @post = Post.find_by(params[:post_id])
-  #   @comment = @post.comments.find(params[:id])
+  def destroy
+    Rails.logger.info("soft delete me aaya")
+    @post = Post.find(params[:post_id])
+    @comment = @post.comments.find(params[:id])
 
-  #   if @comment.update(is_deleted: true)
-  #     redirect_to post_path(@post), notice: "Comment was successfully hidden."
-  #   else
-  #     redirect_to post_path(@post), alert: "Failed to delete comment."
-  #   end
-  # end
+    if @comment.update(is_deleted: true)
+      redirect_to post_path(@post), notice: "Comment was successfully hidden."
+    else
+      redirect_to post_path(@post), alert: "Failed to delete comment."
+    end
+  end
+  
 
   private
+
+  def set_post
+    @post = Post.find(params[:post_id])
+  end
 
   def comment_params
     params.require(:comment).permit(:name, :comment)
